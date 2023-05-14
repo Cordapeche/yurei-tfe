c478c253dbe24241632cf6ee46c6895acdd37c27
Ibrahima <114165898+Cordapeche@users.noreply.github.com>
2023-05-14 15:26:04 +0200

    picture changes

--- public/js/script.js
+++ public/js/script.js
@@ -49,4 +49,40 @@ ready(function () {
       cursor.classList.remove('hover');
     });
   })
+
+  function cookieName (){
+    // Define cookie name
+    var cookieName = 'hide_donate';
+  
+    // Configure fancyBox
+    $('.helpfulwindow').fancybox({
+        autoDimensions: false,
+        autoSize: false,
+        height: 'auto',
+        padding: 0,
+        width: 650,
+        beforeLoad: function() {
+          // Returning false will stop fancybox from opening
+          return ! $.cookie(cookieName);
+        },
+        afterClose: function() {
+          // Set cookie to hide fancybox for 1 day
+          $.cookie(cookieName, true, { expires: 1 });
+        }
+    });
+  
+    // Handle donate click event
+    $('btnokay').on('click', function (event) {
+        event.preventDefault(); 
+  
+        // Hide fancybox and set cookie to hide fancy box for 7 days
+        $.fancybox.close();
+        $.cookie(cookieName, true, { expires: 7 });
+    });
+  
+    // Launch fancyBox on first element
+    setTimeout(function () {
+        $(".btnokay").trigger('click');
+    }, 2000);
+  };
 });
