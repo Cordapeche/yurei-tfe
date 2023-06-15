00df43e14a83b8196b466a724938578fcb3ca2e2
Ibrahima <114165898+Cordapeche@users.noreply.github.com>
2023-06-15 19:48:27 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import Layout from '../layout/layout';
 import '../styles/globals.css';
 import '/styles/yurei.css';
-import { gsap } from 'gsap';
 
- function App({ Component, pageProps }) {
-  let timeline = gsap.timeline();
+ function App({ Component }) {
+
   return (
     <div className='App'>
-      <Layout timeline={timeline}>
-      <Component timeline={timeline} {...pageProps} />
+      <Layout>
+      <Component/>
     </Layout>
     </div>
   )
